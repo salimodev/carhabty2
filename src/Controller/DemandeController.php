f8eac7e6111d0ec906338650ec502a608a77bcdf
salimodev <salimabbessi.dev@gmail.com>
2025-12-11 16:17:29 +0100

    grand mise a jour admin11

--- src/Controller/DemandeController.php
+++ src/Controller/DemandeController.php
@@ -149,9 +149,7 @@ $email
     ->embedFromPath('https://res.cloudinary.com/aladdineshoping/image/upload/v1765309135/logo_essayara_igu3v8.png', 'logo', 'image/png')
     ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681422924/h2qdkkms0lmucs44rc6s.png', 'facebook', 'image/png')
     ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681422987/jtxpgab3dhykivmpw6y5.png', 'instagram', 'image/png')
-    ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681423044/gchpepwboglj5oyudqr8.png', 'twitter', 'image/png')
-    ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681423097/afyzbcguyosyjfbhpn8v.png', 'Linkidin', 'image/png')
-    ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681423192/irpvbr5wdjsewm0i5jgu.png', 'Tick', 'image/png');
+     ->embedFromPath('https://res.cloudinary.com/b-ja/image/upload/v1681423192/irpvbr5wdjsewm0i5jgu.png', 'Tick', 'image/png');
                  
 // Send the email
 $this->mailer->send($email);
